1d43abc2d95d0aca4f7f81441ef531a3d6d377ad
Zhangsk <Zhangsk@softpoweredu.com>
2017-08-29 15:45:34 +0800

    first commit

---
+++ app/src/main/java/com/valueanimator/MainActivity.java
@@ -0,0 +1,24 @@
+package com.valueanimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.valueanimator.view.ImitateKeepButton;
+public class MainActivity extends AppCompatActivity {
+    private ImitateKeepButton imitateKeepButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imitateKeepButton = (ImitateKeepButton) findViewById(R.id.imitateKeepButton);
+        imitateKeepButton.setOnViewClick(new ImitateKeepButton.OnViewClick() {
+            @Override
+            public void onFinish(View view) {
+                Toast.makeText(MainActivity.this,"签到完成！", Toast.LENGTH_SHORT).show();
+                //    imitateKeepButton.setCircleColor(Color.GRAY);
+                imitateKeepButton.setContentText("完成");
+                // imitateKeepButton.setEnabled(false);
+            }
+        });
+    }
+}
